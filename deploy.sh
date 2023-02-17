57362492d5d3ec3628e763a9e351f7e0fc9b90c8
gregmika <grzegorz.mika@relia-sol.pl>
2023-02-18 00:06:39 +0100

    bump Go version to 1.20

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,14 @@ docker push 906350741214.dkr.ecr.eu-north-1.amazonaws.com/go-web-server:latest
 # deploy to GoServer
 ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-13-53-40-61.eu-north-1.compute.amazonaws.com "aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 906350741214.dkr.ecr.eu-north-1.amazonaws.com"
 scp -i "$HOME/.ssh/20230129-aws.pem" ./docker-compose.yml ubuntu@ec2-13-53-40-61.eu-north-1.compute.amazonaws.com:
+ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-13-53-40-61.eu-north-1.compute.amazonaws.com "sudo rm ./logs/webserver.log"
 ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-13-53-40-61.eu-north-1.compute.amazonaws.com "docker compose up -d --pull always"
 
 # deploy to GoSever-001
 ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-16-170-236-95.eu-north-1.compute.amazonaws.com "aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 906350741214.dkr.ecr.eu-north-1.amazonaws.com"
 scp -i "$HOME/.ssh/20230129-aws.pem" ./docker-compose.yml ubuntu@ec2-16-170-236-95.eu-north-1.compute.amazonaws.com:
+ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-16-170-236-95.eu-north-1.compute.amazonaws.com "sudo rm ./logs/webserver.log"
 ssh -i "$HOME/.ssh/20230129-aws.pem" ubuntu@ec2-16-170-236-95.eu-north-1.compute.amazonaws.com "docker compose up -d --pull always"
+
+# cleanup
+docker system prune --force
